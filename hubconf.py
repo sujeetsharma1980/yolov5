344c38a62e8e1b67f9d91ff84b1935e016cbcdfb
Sujeet Sharma <sujeetsharma1980@gmail.com>
2022-01-07 00:03:11 -0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -65,7 +65,7 @@ def _create(name, pretrained=True, channels=3, classes=80, autoshape=True, verbo
         raise Exception(s) from e
 
 
-def custom(path='path/to/model.pt', autoshape=True, verbose=True, device=None):
+def custom(path='/temp/best.pt', autoshape=True, verbose=True, device=None):
     # YOLOv5 custom or local model
     return _create(path, autoshape=autoshape, verbose=verbose, device=device)
 
@@ -121,8 +121,8 @@ def yolov5x6(pretrained=True, channels=3, classes=80, autoshape=True, verbose=Tr
 
 
 if __name__ == '__main__':
-    model = _create(name='yolov5s', pretrained=True, channels=3, classes=80, autoshape=True, verbose=True)  # pretrained
-    # model = custom(path='path/to/model.pt')  # custom
+    #model = _create(name='yolov5s', pretrained=True, channels=3, classes=80, autoshape=True, verbose=True)  # pretrained
+    model = custom(path='/temp/best.pt')  # custom
 
     # Verify inference
     from pathlib import Path
